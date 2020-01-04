1f0f97a0b3bc8fc5c281d9a0d0c79c9fbcba5b08
John <beerhunter133@gmail.com>
2020-01-04 19:02:43 +0200

    first tr commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+// console.log(persone["name"]);
+
+// let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+// console.log(arr[1]);
+
+// alert("topagent");
+// let answer = prompt("хер длиной 18?", "Да");
+// console.log(typeof(answer));
+// console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
